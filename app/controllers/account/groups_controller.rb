1ccd2688e71da1c325cb1651c9b63e95910bca0d
te <wjt18610385640@gmail.com>
2017-05-09 07:41:29 +0800

    use simple_format helper to decorate

--- app/controllers/account/groups_controller.rb
+++ app/controllers/account/groups_controller.rb
@@ -4,4 +4,8 @@ class Account::GroupsController < ApplicationController
   def index
     @groups = current_user.participated_groups
   end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
 end
--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -3,4 +3,8 @@ class Account::PostsController < ApplicationController
   def index
     @posts = current_user.posts
   end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
 end
